54eaed066f25b2791fb63f0511a43c91a56bd49c
Jonathan <jonretchless@gmail.com>
2020-05-21 19:55:28 -0700

    changed client url

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/groupcheck',
   //deploying a db is separate, will need to change the database url once the db is deployed
-  CLIENT_ORIGIN: "*"
+  CLIENT_ORIGIN: "https://groupcheck.jonretchless.now.sh"
 }
